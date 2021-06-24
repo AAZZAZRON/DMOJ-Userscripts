765307110a8d677e34076c66c4cce14daac232d3
AAZZAZRON <72463938+AAZZAZRON@users.noreply.github.com>
2021-06-24 19:58:36 -0400

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,62 @@
+// put friend's names (case sensitive) in friends
+var friends = [];
+
+$(document).ready(function() {
+    if ($("li.tab.active")[0].innerText == " Rankings") {
+        var insert = document.querySelector("#content-left > div");
+        
+        // show live participants
+        var lbLive = document.createElement("input");
+        lbLive.setAttribute("type", "checkbox");
+        var tag = document.createElement("label");
+        tag.innerText = "Show Live Participants ";
+        insert.appendChild(lbLive);
+        insert.appendChild(tag);
+        lbLive.checked = false;
+        
+        // private leaderboard
+        var lbPrivate = document.createElement("input");
+        var tag2 = document.createElement("label");
+        lbPrivate.setAttribute("type", "checkbox");
+        tag2.innerText = "Show Private Leaderboard ";
+        insert.appendChild(lbPrivate);
+        insert.appendChild(tag2);
+        lbPrivate.checked = false;
+        var elements = $("tbody")[0].children;
+
+        // live
+        lbLive.onclick = commands;
+        
+        // friends
+        lbPrivate.onclick = commands;
+
+        
+        function commands() {
+            if (lbLive.checked && lbPrivate.checked) {
+                showLive();
+                showLB();
+                return;
+            } 
+            reset();
+            if (lbLive.checked) showLive();
+            else if (lbPrivate.checked) showLB();
+        }
+        
+        function showLive() {
+            for (var i = 0; i < elements.length ; i += 1) { // remove not live
+                if ($("#" + elements[i].id + " > td.user-name > div")[0].innerText == "Participation ended.") elements[i].style.display = "none";
+            }
+        }
+        
+        function showLB() {
+            for (var i = 0; i < elements.length ; i += 1) { // remove not friend users
+                var user = elements[i].id.substring(5);
+                if (!friends.includes(user)) elements[i].style.display = "none";
+            }
+        }
+        
+        function reset() {
+            for (var i = 0; i < elements.length; i += 1) elements[i].style.display = "";
+        }
+    }
+})
