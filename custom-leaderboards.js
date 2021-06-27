f1f95a2a375e25c9837ea1b0dd62dee49d050e7e
AAZZAZRON <72463938+AAZZAZRON@users.noreply.github.com>
2021-06-27 09:32:29 -0400

    new add friends

--- custom-leaderboards.js
+++ custom-leaderboards.js
@@ -2,13 +2,14 @@
 // one can also see live participants as a separate leaderboard
 // to customize the customizable leaderboard, simply write a user's handle in the variable "friends" (line 8)
 // to see the leaderboard, simply click the checkbox on the rankings page
+// add friends by going to their profile and clicking "Add Friend"
 
 
-// put friend's DMOJ handles (case sensitive) in friends
 var friends = [];
 
 $(document).ready(function() {
     if ($("li.tab.active")[0].innerText == " Rankings") {
+        friends = getCookie("friends").split("!fRiEnDs!");
         var insert = document.querySelector("#content-left > div");
         
         // show live participants
@@ -63,6 +64,82 @@ $(document).ready(function() {
         
         function reset() {
             for (var i = 0; i < elements.length; i += 1) elements[i].style.display = "";
+        }
+    }
+})
+
+// cookie functions
+function getCookie(name) {
+    var find = name + "=";
+    var things = document.cookie.split("; ");
+    for (let i = 0; i < things.length; i += 1) {
+        var found = true;
+        for (let j = 0; j < find.length; j += 1) {
+            if (things[i][j] != find[j]) {
+                found = false;
+                break;
+            }
+        }
+        if (found) {
+            return things[i].split(find)[1]; // returns value
+        }
+    }
+    return "";
+}
+
+function addCookie(name, value) {
+    var expire = "expires=Fri, 3 Jan 3000 23:59:59 GMT";
+    document.cookie = `${name}=${value}; ${expire};path=/`;
+}
+
+// adding and removing friends
+$(document).ready(function() {
+    friends = getCookie("friends").split("!fRiEnDs!");
+    if (!friends.includes($("span > b")[0].innerText)) {
+        friends.push($("span > b")[0].innerText);
+        addCookie("friends", friends.join("!fRiEnDs!"));
+    }
+    if ($("li.tab.active > a")[0].innerText == " About") {
+        console.log(friends);
+        var user = $("h2")[0].innerText;
+        if (user !== "My account") {
+            // add checkbox
+            user = user.split("User ")[1];
+            console.log(user);
+            var isFriend = document.createElement("input");
+            isFriend.setAttribute("type", "checkbox");
+            var tag = document.createElement("label");
+            tag.innerText = "Is Friend \n";
+            var tmp = $("div.user-sidebar")[0];
+            tmp.insertBefore(isFriend, tmp.children[5]);
+            tmp.insertBefore(tag, tmp.children[6]);
+            if (friends.includes(user)) {
+                isFriend.checked = true;
+            }
+            isFriend.onclick = updateFriends; // update cookie commands
+            
+            function updateFriends() {
+                if (isFriend.checked) addFriend();
+                else removeFriend();
+            }
+            
+            function addFriend() {
+                friends = getCookie("friends").split("!fRiEnDs!");
+                friends.push(user);
+                addCookie("friends", friends.join("!fRiEnDs!"));
+                alert(user + " has been friended.");
+            }
+        
+            function removeFriend() {
+                for (var i = 0; i < friends.length; i++) { 
+                    if (friends[i] === user) { 
+                        friends.splice(i, 1); 
+                        i--; 
+                    }
+                }
+                addCookie("friends", friends.join("!fRiEnDs!"));
+                alert(user + " is no longer your friend.");
+            }
         }
     }
 })
