93ba01c2dfed60f717d99fa81f0246f45dbf7ad5
Aaron Zhu <72463938+AAZZAZRON@users.noreply.github.com>
2021-09-18 08:33:58 -0400

    Update userscript.js

--- Remove Editorials/userscript.js
+++ Remove Editorials/userscript.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
     if (window.location.href.includes("/problem/") && $('.info-float > div')[3].innerText == 'Read editorial') {
         $('.info-float > div')[3].remove();
         $('.info-float > hr')[1].remove();
-        console.log("Has editorial");
+        console.log("Yes editorial");
+    } else {
+        console.log("No editorial");
     }
 })
