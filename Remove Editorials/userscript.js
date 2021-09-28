090a78279697a524bd99641fda8fb633050f88fa
Aaron Zhu <72463938+AAZZAZRON@users.noreply.github.com>
2021-09-28 07:46:56 -0400

    Update userscript.js

--- Remove Editorials/userscript.js
+++ Remove Editorials/userscript.js
@@ -1,9 +1,13 @@
 // remove editorials
 $(document).ready(function() {
-    if (window.location.href.includes("/problem/") && $('.info-float > div')[3].innerText == 'Read editorial') {
+    if (window.location.href.includes("/problem/") && $('.info-float > div')[3].innerText == 'Read editorial') { // if you have submissions
         $('.info-float > div')[3].remove();
         $('.info-float > hr')[1].remove();
         console.log("Yes editorial");
+    } else if (window.location.href.includes("/problem/") && $('.info-float > div')[2].innerText == 'Read editorial') { // if you don't have submissions
+        $('.info-float > div')[2].remove();
+        $('.info-float > hr')[1].remove();
+        console.log("Yes editorial");   
     } else {
         console.log("No editorial");
     }
